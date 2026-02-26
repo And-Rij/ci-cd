c7795fe99c0b6242e03169aa9fc8869e0b6dfce4
And-Rij <andrijsupa@gmail.com>
2026-02-26 16:51:22 +0200

    add about page with error

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import { CardExample } from "@/components/card-exaple";
+import { useState } from "react";
+
+
+export default function Page() {
+    const [asd, setAsd] = useState(false)
+
+    return <CardExample />
+}
