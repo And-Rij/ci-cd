369eb4c3d9bba7aaa996fc40097e302cea42200f
And-Rij <andrijsupa@gmail.com>
2026-02-26 16:52:59 +0200

    remove error

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,6 @@
 import { CardExample } from "@/components/card-exaple";
-import { useState } from "react";
 
 
 export default function Page() {
-    const [asd, setAsd] = useState(false)
-
     return <CardExample />
 }
